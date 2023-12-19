3e6fa8aefba6411ebd21b82370b2ab9c5b692ef8
María José Peña Ruiz <maryjop10@gmail.com>
2023-12-19 11:10:57 +0100

    changes to floor

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import { NavLink as Link } from "react-router-dom";
+import './navbar.css'
 
 const Navbar = () => {
     return(
-        <div>
-            <Link to='/'>
+        <div className="navbar">
+            <Link className="navbar-item" to='/'>
                 <h1>Home</h1>
             </Link>
-            <Link to='/about'>
+            <Link className="navbar-item" to='/about'>
                 <h1>About Me</h1>
             </Link>
-            <Link to='/project'>
+            <Link className="navbar-item" to='/project'>
                 <h1>Projects</h1>
             </Link>
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,43 +2,49 @@ import React, { useEffect, useState } from "react"
 import '../styles/home.css'
 
 const Home = () => {
-    const [numLines, setNumLines] = useState(0);
 
-    useEffect(() => {
-      const calculateNumLines = () => {
-        // Obtén el ancho de la pantalla
-        const screenWidth = window.innerWidth;
-  
-        // Calcula el número de líneas que caben en la pantalla
-        const linesFitInScreen = Math.floor(screenWidth / 150); // 30 es el ancho estimado de una línea
-  
-        setNumLines(linesFitInScreen);
-      };
-  
-      // Llama a la función para calcular el número de líneas al montar el componente y cuando cambia el tamaño de la pantalla
-      calculateNumLines();
-      window.addEventListener('resize', calculateNumLines);
-  
-      // Limpia el event listener al desmontar el componente
-      return () => {
-        window.removeEventListener('resize', calculateNumLines);
-      };
-    }, []);
-  
-    const generateLines = () => {
-      const lines = [];
-      for (let i = 0; i < numLines; i++) {
-        lines.push(<div key={i} className="vertical-line"></div>);
-      }
-      return lines;
-    };
-  
-    return (
-      <div className="home-page">
-        <h1>Bienvenido a nuestra página de inicio</h1>
-        <div className="vertical-lines">{generateLines()}</div>
+  return (
+    <div className="home-page">
+
+
+      <div className="title-container">
+        <h1 className="neon-text">Página de Inicio</h1>
+        <p className="subtitle">¡Explora el mundo gaming neon!</p>
       </div>
-    );
-  };
+
+      <div className="button-container">
+        <button className="neon-button">Botón 1</button>
+        <button className="neon-button">Botón 2</button>
+        <button className="neon-button">Botón 3</button>
+      </div>
+      <div className="floor-container">
+        <div className="floor-left">
+
+        </div>
+        <div className="floor-center">
+
+        </div>
+        <div className="floor-right">
+
+        </div>
+        <div className="grid-container">
+          {/* Líneas horizontales */}
+          <div className="grid-line"></div>
+          <div className="grid-line"></div>
+          <div className="grid-line"></div>
+          {/* ... otras líneas horizontales ... */}
+
+          {/* Líneas verticales */}
+          <div className="grid-line vertical"></div>
+          <div className="grid-line vertical"></div>
+          <div className="grid-line vertical"></div>
+          <div className="grid-line vertical"></div>
+          <div className="grid-line vertical"></div>
+          {/* ... otras líneas verticales ... */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
