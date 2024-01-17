b4d4957778ec6101f3f22390146800dd12e1df01
María José Peña Ruiz <maryjop10@gmail.com>
2024-01-17 18:28:55 +0100

    floor grid finished

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,20 +13,20 @@ const Home = () => {
             style={{ top: `${(index + 1) * 10}vh` }}
           ></div>
         ))}
-        {[...Array(5).keys()].map((_, index) => (
-          <div
-            key={`vertical-left-${index}`}
-            className="line-vertical line-left"
-            style={{ left: `${(index + 1) * 10}vw` }}
-          ></div>
-        ))}
-        {[...Array(5).keys()].map((_, index) => (
-          <div
-            key={`vertical-right-${index}`}
-            className="line-vertical line-right"
-            style={{ right: `${(index + 1) * 10}vw` }}
-          ></div>
-        ))}
+          {[...Array(5).keys()].map((_, index) => (
+            <div
+              key={`vertical-left-${index}`}
+              className="line-vertical line-left"
+              style={{ left: `${(index + 1) * 10}vw` }}
+            ></div>
+          ))}
+          {[...Array(5).keys()].map((_, index) => (
+            <div
+              key={`vertical-right-${index}`}
+              className="line-vertical line-right"
+              style={{ right: `${(index + 1) * 10}vw` }}
+            ></div>
+          ))}
       </div>
     </>
   );
